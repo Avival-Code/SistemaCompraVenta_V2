ea1e30f4797b8532801af75f1f9a6f9f96eac9c1
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-08 14:06:52 -0500

    Implementacion parcial de fragmento "perfil_fragment". Cree el controlador del fragmento "modificar_datos_fragment".

---
+++ app/src/main/java/com/example/sistemacompraventa_v2/controladores/ModificarDatosFragmento.java
@@ -0,0 +1,29 @@
+package com.example.sistemacompraventa_v2.controladores;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.sistemacompraventa_v2.R;
+
+public class ModificarDatosFragmento extends Fragment implements View.OnClickListener {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState ) {
+        return null;
+    }
+
+    @Override
+    public void onClick( View view ) {
+        switch( view.getId() ) {
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sistemacompraventa_v2/controladores/PerfilFragmento.java
@@ -0,0 +1,76 @@
+package com.example.sistemacompraventa_v2.controladores;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.sistemacompraventa_v2.R;
+import com.example.sistemacompraventa_v2.sesionusuario.LoginSession;
+import com.example.sistemacompraventa_v2.utilities.ApiRequests;
+import com.example.sistemacompraventa_v2.utilities.StringValidator;
+
+public class PerfilFragmento extends Fragment implements View.OnClickListener{
+    private View perfilView;
+    private TextView nombres;
+    private TextView apellidos;
+    private TextView correo;
+    private TextView telefono;
+    private Button modificarDatos;
+    private Button agregarDomicilio;
+
+    @Nullable
+    @Override
+    public View onCreateView( @NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState ) {
+        perfilView = inflater.inflate( R.layout.perfil_fragment, container, false );
+
+        modificarDatos = ( Button )perfilView.findViewById( R.id.modificar_datos_button );
+        agregarDomicilio = ( Button )perfilView.findViewById( R.id.agregar_domicilio_button );
+        nombres = ( TextView ) perfilView.findViewById( R.id.usuario_nombres_text );
+        apellidos = ( TextView ) perfilView.findViewById( R.id.usuario_apellidos_text );
+        correo = ( TextView ) perfilView.findViewById( R.id.usuario_correo_text );
+        telefono = ( TextView ) perfilView.findViewById( R.id.usuario_telefono_text );
+        modificarDatos.setOnClickListener( this );
+        agregarDomicilio.setOnClickListener( this );
+
+        return perfilView;
+    }
+
+    @Override
+    public void onClick( View view ) {
+        switch( view.getId() ) {
+            case R.id.modificar_datos_button:
+                showModifyInfoScreen();
+                break;
+
+            case R.id.agregar_domicilio_button:
+                break;
+        }
+    }
+
+    private void showModifyInfoScreen() {
+        final FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace( R.id.Fragment_container, new ModificarDatosFragmento() ).commit();
+    }
+
+    private void showAddAddressScreen() {
+
+    }
+
+    private void loadUserInfo() {
+        if( LoginSession.GetInstance().getUsuario() == null ) {
+
+        }
+        nombres.setText( LoginSession.GetInstance().getUsuario().getNombres() );
+        apellidos.setText( LoginSession.GetInstance().getUsuario().getApellidos() );
+        correo.setText( LoginSession.GetInstance().getUsuario().getCorreoElectronico() );
+        telefono.setText( LoginSession.GetInstance().getUsuario().getTelefono() );
+    }
+}

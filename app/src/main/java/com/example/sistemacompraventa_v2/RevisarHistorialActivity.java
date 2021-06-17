a348328992236fc268eed24190e9678778caa430
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-17 15:39:22 -0500

    Creacion de la actividad RevisarHistorialActivity. Agregue un recyclerview a su archivo xml correspondiente.

---
+++ app/src/main/java/com/example/sistemacompraventa_v2/RevisarHistorialActivity.java
@@ -0,0 +1,14 @@
+package com.example.sistemacompraventa_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RevisarHistorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.revisar_historial_activity );
+    }
+}

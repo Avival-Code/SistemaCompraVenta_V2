4ec7f1a06bbe6497eabb8f72711871c652232946
AlanMorgaMora98 <62389250+AlanMorgaMora98@users.noreply.github.com>
2021-06-19 20:34:10 -0500

    Creacion de archivo xml publicacion_fragment.xml para el controlador PublicacionFragmento. Este fragmento sera utilizado para navegar a la activiadd de crear Publicaciones y al fragmento para ver todas las publicaciones del usuario actual.

--- app/src/main/java/com/example/sistemacompraventa_v2/controladores/PublicacionFragmento.java
+++ app/src/main/java/com/example/sistemacompraventa_v2/controladores/PublicacionFragmento.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,12 +12,28 @@ import androidx.fragment.app.Fragment;
 
 import com.example.sistemacompraventa_v2.R;
 
-public class PublicacionFragmento extends Fragment{
+public class PublicacionFragmento extends Fragment implements View.OnClickListener{
     private View publicacionView;
+    private Button crearPublicacion;
+    private Button verPublicaciones;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState ) {
         publicacionView = inflater.inflate(R.layout.perfil_fragment, container, false);
 
+        crearPublicacion = publicacionView.findViewById( R.id.crear_publicacion_button );
+        verPublicaciones = publicacionView.findViewById( R.id.ver_publicaciones_button );
+
         return publicacionView;
+    }
+
+    @Override
+    public void onClick( View view ) {
+        switch( view.getId() ) {
+            case R.id.crear_publicacion_button:
+                break;
+
+            case R.id.ver_publicaciones_button:
+                break;
+        }
     }
 }
